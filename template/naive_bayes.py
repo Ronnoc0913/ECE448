86dcb5050a829486ad3e7eb45d986229074f733c
Ronnoc0913 <cctan2@illinois.edu>
2025-02-04 17:05:09 -0600

    Finished MP1

--- template/naive_bayes.py
+++ template/naive_bayes.py
@@ -50,8 +50,56 @@ Main function for training and predicting with naive bayes.
 def naive_bayes(train_labels, train_data, dev_data, laplace=1.0, pos_prior=0.5, silently=False):
     print_values(laplace,pos_prior)
 
-    yhats = []
-    for doc in tqdm(dev_data, disable=silently):
-        yhats.append(-1)
+    # Count words in positive and negative reviews
+    word_count_pos = Counter()
+    word_count_neg = Counter()
 
-    return yhats
+    total_words_pos = 0
+    total_words_neg = 0
+
+    for i in range (len(train_data)):
+        if train_labels[i] == 1:
+            word_count_pos.update(train_data[i])
+            total_words_pos += len(train_data[i])
+        else:
+            word_count_neg.update(train_data[i])
+            total_words_neg += len(train_data[i])
+
+    # Create vocabulary and compute probabilities in advance.
+    vocabulary = set(word_count_pos.keys()).union(set(word_count_neg.keys()))
+    vocab_size = len(vocabulary)
+
+    p_word_given_pos = {
+        word: math.log((word_count_pos[word] + laplace) / (total_words_pos + laplace * vocab_size))
+        for word in vocabulary
+    }
+
+    p_word_given_neg = {
+        word: math.log((word_count_neg[word] + laplace) / (total_words_neg + laplace * vocab_size))
+        for word in vocabulary
+    }
+
+    # Handling unseen words
+    unseen_prob_pos = math.log(laplace / (total_words_pos + laplace * vocab_size))
+    unseen_prob_neg = math.log(laplace / (total_words_neg + laplace * vocab_size))
+
+    # Compute Prior Probabilities (in log space)
+    log_prior_pos = math.log(pos_prior)
+    log_prior_neg = math.log(1 - pos_prior)
+
+    # Classify Development Data
+    yhats = []
+    for review in tqdm(dev_data, disable=silently):
+        log_prob_pos = log_prior_pos
+        log_prob_neg = log_prior_neg
+
+        for word in review:
+            log_prob_pos += p_word_given_pos.get(word, unseen_prob_pos)
+            log_prob_neg += p_word_given_neg.get(word, unseen_prob_neg)
+
+        # Choose the label with the higher probability
+        predicted_label = 1 if log_prob_pos > log_prob_neg else 0
+        yhats.append(predicted_label)
+
+    return yhats 
+
